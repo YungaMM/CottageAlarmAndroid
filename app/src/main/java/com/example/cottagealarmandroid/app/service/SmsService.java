4590e410559ee0343a17fe8f73981a063467b870
Yungamm <yungamm@yandex.ru>
2016-02-05 11:59:09 +0300

    Начало разработки Сервиса обработки СМС

---
+++ app/src/main/java/com/example/cottagealarmandroid/app/service/SmsService.java
@@ -0,0 +1,42 @@
+package com.example.cottagealarmandroid.app.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+public class SmsService extends Service {
+    final String LOG_TAG = "myLogs";
+
+    public SmsService() {
+    }
+
+    public void onCreate() {
+        super.onCreate();
+        Log.d(LOG_TAG, "onCreate");
+    }
+
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d(LOG_TAG, "onStartCommand");
+        someTask();
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(LOG_TAG, "onDestroy");
+    }
+
+    public IBinder onBind(Intent intent) {
+        Log.d(LOG_TAG, "onBind");
+        return null;
+    }
+
+    void someTask() {
+    }
+//    @Override
+//    public IBinder onBind(Intent intent) {
+//        // TODO: Return the communication channel to the service.
+//        throw new UnsupportedOperationException("Not yet implemented");
+//    }
+}
